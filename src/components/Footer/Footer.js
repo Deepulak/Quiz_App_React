cf73495b26adaf9116bed5a69b11a599dfdaccb6
@Deecoder <45496136+Deepulak@users.noreply.github.com>
2022-02-09 18:48:23 +0600

    first changes
    for this simple quiz app

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,20 @@
+const Footer = () => {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        marginBottom: 10,
+      }}
+    >
+      Made with ♥ by{" "}
+      <a
+        href="https://www.youtube.com/channel/UCbRH_m9qoOxYggiUUgx-j1w"
+        style={{ cursor: "pointer" }}
+      >
+        Декодер
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
